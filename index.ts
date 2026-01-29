b21305131bf1517de604a64ea901b79f22815e0c
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-01-29 11:55:11 -0500

    add run Command

--- index.ts
+++ index.ts
@@ -5,3 +5,14 @@ const rl:any = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+function run(command:string) {
+  try {
+    console.log(`Führe aus: ${command}`);
+    // stdio: 'inherit' sorgt dafür, dass du den Output (z.B. Git-Status) direkt siehst
+    execSync(command, { stdio: 'inherit' });
+  } catch (error) {
+    console.error(`Fehler beim Befehl: ${command}`);
+    process.exit(1);
+  }
+}
