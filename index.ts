279cc7f370ee4c438e324e73da5e890bb7dafb15
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-01-29 12:23:06 -0500

    fixed

--- index.ts
+++ index.ts
@@ -1,16 +1,14 @@
 const { execSync } = require("child_process");
 const readline = require("readline");
-let text_info: string;
-let message: string;
 
 console.log('\n--- Starte Git Workflow ---');
 
-const rl: any = readline.createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-function run(command: string) {
+function run(command:string) {
   try {
     console.log(`Run command: ${command}`);
     execSync(command, { stdio: "inherit" });
@@ -20,26 +18,13 @@ function run(command: string) {
   }
 }
 
-function input(text_info: string) {
-  rl.question(text_info, (message: string) => {
+function input(text_info:string) {
+  rl.question(text_info, (message:string) => {
     return message;
   });
 }
 
-
-// }
-// rl.question('Gib deine Commit-Nachricht ein: ', (message) => {
-
-//   console.log('\n--- Starte Git Workflow ---');
-
 //   run('git init');
 //   run('git add .');
-
-//   // Hier nutzen wir die Benutzereingabe für den Commit
 //   run(`git commit -m "${message}"`);
-
 //   console.log('\nFertig! Der Commit wurde erstellt.');
-
-//   rl.close();
-// });
-
