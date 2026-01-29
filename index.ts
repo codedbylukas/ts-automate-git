3d76ea87470fdc16ed1d182a8a773a18475c3af5
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-01-29 11:52:33 -0500

    add any

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 const { execSync } = require('child_process');
 const readline = require('readline');
 
-const rl = readline.createInterface({
+const rl:any = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
