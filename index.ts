a8fbc245674aafe7537595626f047bf82cef641e
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-01-29 12:10:42 -0500

    add imput

--- index.ts
+++ index.ts
@@ -1,17 +1,25 @@
-const { execSync } = require('child_process');
-const readline = require('readline');
+const { execSync } = require("child_process");
+const readline = require("readline");
+let text_info: string;
+let message: string;
 
-const rl:any = readline.createInterface({
+const rl: any = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-function run(command:string) {
+function run(command: string) {
   try {
     console.log(`Run command: ${command}`);
-    execSync(command, { stdio: 'inherit' });
+    execSync(command, { stdio: "inherit" });
   } catch (error) {
     console.error(`Error: ${command}`);
     process.exit(1);
   }
+}
+
+function input(text_info: string) {
+  rl.question(text_info, (message: string) => {
+    return message;
+  });
 }
