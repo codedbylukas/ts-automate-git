fdf7956edc9f192317214662b80b53b3de7c76d0
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-01-31 02:13:16 -0500

    splited into multible files

--- index.ts
+++ index.ts
@@ -1,35 +1,10 @@
-import { execSync } from "child_process";
-import readline from "readline";
+import { run } from "./run.js";
+import { input } from "./input.js";
 
 let push:string;
 let init:string;
 
-export {}; 
-
 console.log('\n--- Starte Git Workflow ---');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-function run(command:string) {
-  try {
-    console.log(`Run command: ${command}`);
-    execSync(command, { stdio: "inherit" });
-  } catch (error) {
-    console.error(`Error: ${command}`);
-    process.exit(1);
-  }
-}
-
-function input(text_info: string): Promise<string> {
-  return new Promise((resolve) => {
-    rl.question(text_info, (message: string) => {
-      resolve(message);
-    });
-  });
-}
 
 init = await input("Do you want to init it? (y/n) ");
 
---
+++ input.ts
@@ -0,0 +1,18 @@
+import readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+export function input(text_info: string): Promise<string> {
+  return new Promise((resolve) => {
+    rl.question(text_info, (message: string) => {
+      resolve(message);
+    });
+  });
+}
+
+export function closeInput() {
+    rl.close();
+}
