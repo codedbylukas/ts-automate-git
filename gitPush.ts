7c02db2a4b714a36dd09267410385161553d3cda
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-01-31 07:10:48 -0500

    add default= y

--- gitPush.ts
+++ gitPush.ts
@@ -1,9 +1,9 @@
 import { input } from "./input.js";
 
 export async function gitPushConfig(): Promise<boolean> {
-  const push = await input("Do you want to push it after every modifire? (y/n) ");
+  const push = await input("Do you want to push it after every modifire? (y/n) (default: y)");
   console.log(push);
-  if (push.trim().toLowerCase() === "y") {
+  if (push.trim().toLowerCase() === "y" || push.trim().toLowerCase() === "") {
     return true;
   } else if (push.trim().toLowerCase() === "n") {
     return false;
