6d9de6482d98f41e1524672b593a7739b5ce744d
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-01-31 15:47:10 -0500

    maked much

--- gitBranch.ts
+++ gitBranch.ts
@@ -2,8 +2,32 @@ import { input } from "./input.js";
 import { run } from "./run.js";
 
 let branchName:string;
+let choice:string;
+
+function showBegining() {
+  console.log("\n--- Git Branch ---");
+  console.log("Do you want to switch or create a new branch? (default: create)");
+  console.log("1. Switch to a branch");
+  console.log("2. Create a new branch");
+}
 
 export async function gitBranch() {
+  showBegining();
+  choice = await input("Enter your choice: ");
+  if (choice.trim() === "1") { 
+    branchName = await input("Enter your branch name: ");
+    if (branchName.trim() === "") {
+      console.log("Branch name cannot be empty");
+      return;
+    }
+    try {
+      run("git switch " + branchName);
+      run("git branch");
+    } catch (error) {
+      console.log("Error: " + error);
+      return;
+    }
+}else if (choice.trim() === "2") {
     branchName = await input("Enter your branch name: ");
     if (branchName.trim() === "") {
       console.log("Branch name cannot be empty");
@@ -15,5 +39,6 @@ export async function gitBranch() {
     } catch (error) {
       console.log("Error: " + error);
       return;
-    }
+    }  
+  }
 }
