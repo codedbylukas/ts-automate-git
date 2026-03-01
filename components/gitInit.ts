287db4c2504d56901b86b4c54e226fdc7334dd18
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-01 06:35:29 +0100

    fixed

--- components/gitInit.ts
+++ components/gitInit.ts
@@ -2,13 +2,19 @@ import { input } from "./input.js";
 import { run } from "./run.js";
 
 export async function gitInit() {
-  const init = await input("Do you want to init it? (y/n) (default: n) ");
-  if (init.trim().toLowerCase() === "y") {
-    run('git init');
-  } else if (init.trim().toLowerCase() === "n" || init.trim() === "") {
-    console.log("Okay, I don't init it.");
-  } else {
-    console.log("Invalid Choice");
-    process.exit();
+  try {
+    const init = await input("Do you want to init it? (y/n) (default: n) ");
+    if (init.trim().toLowerCase() === "y") {
+      run('git init');
+    } else if (init.trim().toLowerCase() === "n" || init.trim() === "") {
+      console.log("Okay, I don't init it.");
+    } else {
+      console.log("Invalid Choice");
+      process.exit();
+    }
+  }
+  catch (e) {
+    console.error("An error occurred while initializing git:", e);
+    process.exit(1);
   }
 }
