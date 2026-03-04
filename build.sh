d4fa476af45bf574b780de66e3c4a728c476f50a
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-04 11:41:14 -0500

    make all

--- build.sh
+++ build.sh
@@ -2,4 +2,7 @@
 
 npm run build
 cp -r ./build/* ./js/
+npx bun build ./index.ts --compile --outfile git-tool
+cp git-tool ./Linux/usr/bin/git-tool
+dpkg-deb --root-owner-group --build Linux
 npm run test
