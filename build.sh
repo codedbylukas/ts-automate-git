a51450e5fabf11082b5d514ea62d1efab4c33711
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-04 12:25:56 -0500

    Build and prepare for installation

--- build.sh
+++ build.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-
+echo "Starting Build-Prozess..."
 npm run build
 npm run test
-cp -r ./build/* ./js/
+
+echo "Compiling Binary with Bun..."
 npx bun build ./index.ts --compile --outfile git-tool
+
+mkdir -p ./Linux/usr/bin/
+mkdir -p for_installation/Linux/
+
 cp git-tool ./Linux/usr/bin/git-tool
+
+echo "build Debian-package..."
 dpkg-deb --root-owner-group --build Linux
-rm for_installation/Linux/Linux.deb for_installation/Linux/git-tool
+
+rm -f for_installation/Linux/Linux.deb for_installation/Linux/git-tool
 mv Linux.deb for_installation/Linux/Linux.deb 
 mv git-tool for_installation/Linux/git-tool
+
+echo "analyzing Code-Menge..."
+cloc . --exclude-dir=node_modules,build,dist --not-match-f='package-lock.json|package.json' > count_code.txt
+
+echo "Synchronisiere with Git..."
 git add .
 git commit -m "Build and prepare for installation"
-git pull
+git pull --rebase
 git push
+
+echo "Finish! Package is in for_installation/Linux/Linux.deb"
--- cloc.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-
-cloc . --exclude-dir=node_modules,build,dist --not-match-f='package-lock.json|package.json' > count_code.txt
