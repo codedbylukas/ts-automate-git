33e90b1f10d395a6056dcc5a1ebc50bdb8601cb5
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-04 12:11:47 -0500

    Build and prepare for installation

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+
 npm run build
+npm run test
 cp -r ./build/* ./js/
 npx bun build ./index.ts --compile --outfile git-tool
 cp git-tool ./Linux/usr/bin/git-tool
 dpkg-deb --root-owner-group --build Linux
-npm run test
+rm for_installation/Linux/Linux.deb for_installation/Linux/git-tool
+mv Linux.deb for_installation/Linux/Linux.deb 
+mv git-tool for_installation/Linux/git-tool
+git add .
+git commit -m "Build and prepare for installation"
+git pull
+git push
