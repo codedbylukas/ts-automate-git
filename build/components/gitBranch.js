195a1236303b6d807f4422f580a7cb749c5635fe
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-04 12:40:18 -0500

    Build and prepare for installation

---
+++ build/components/gitBranch.js
@@ -0,0 +1,67 @@
+import { input } from "./input.js";
+import { run } from "./run.js";
+let branchName;
+let choice;
+function showBegining() {
+    console.log("\n--- Git Branch ---");
+    console.log("Do you want to switch or create a new branch? (default: create)");
+    console.log("1. Switch to a branch");
+    console.log("2. Create a new branch");
+}
+async function switchBranch() {
+    branchName = await input("Enter your branch name: ");
+    if (branchName.trim() === "") {
+        console.log("Branch name cannot be empty");
+        return;
+    }
+    try {
+        run("git switch " + branchName);
+        run("git branch");
+        process.exit();
+    }
+    catch (e) {
+        console.error("Error: " + e);
+        return;
+    }
+}
+async function createBranch(pushing) {
+    branchName = await input("Enter your branch name: ");
+    if (branchName.trim() === "") {
+        console.log("Branch name cannot be empty");
+        return;
+    }
+    try {
+        run("git switch -c " + branchName);
+        if (pushing) {
+            try {
+                run("git push -u origin " + branchName);
+            }
+            catch (e) {
+                console.error("Error pushing branch: " + e);
+                return;
+            }
+        }
+        run("git branch");
+        process.exit();
+    }
+    catch (error) {
+        console.log("Error: " + error);
+        return;
+    }
+}
+export async function gitBranch(pushing) {
+    try {
+        showBegining();
+        choice = await input("Enter your choice (1/2) (default: 2): ");
+        if (choice.trim() === "1") {
+            switchBranch();
+        }
+        else if (choice.trim() === "2") {
+            createBranch(pushing);
+        }
+    }
+    catch (e) {
+        console.error("Error: " + e);
+        return Promise.reject(e);
+    }
+}
