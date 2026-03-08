ecbea4f5270cc4237a7b2c731587037f928ad46b
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-08 11:26:34 -0400

    Build and prepare for installation

--- build/components/run.js
+++ build/components/run.js
@@ -5,8 +5,9 @@ export function run(command) {
         execSync(command, { stdio: "inherit" });
     }
     catch (e) {
-        console.error(`Error: ${command}`);
+        console.error(`Error : ${command}`);
         console.error(`Error details: ${e}`);
+        console.error("Error IN RUN FILE: " + e);
         process.exit(1);
     }
 }
